6108c54101c0ba8fdbcb112e9faa604d979151fb
zkl <zkl.1880@gmail.com>
2014-03-10 20:37:14 +0800

    去掉不必要的测试内容

--- webspider/webspider/main.cpp
+++ webspider/webspider/main.cpp
@@ -3,19 +3,11 @@
 #include <stdio.h>
 #include "spider.h"
 
-const char * test1 = "http://53359.com/";
-const char * test2 = "bbs8899.net/Sort.aspx?number=3";
-
 const char * host[] = {
-	"53359.com",
 	"www.baidu.com",
 	"www.google.com",
 	"www.iqiyi.com",
 	"www.youku.com",
-	"www.yunio.com",
-	"smtp.qq.com:25",
-	"http://www.baidu.com/index.html",
-	"http://www.kerneltravel.net/",
 };
 
 int main(int argc, char *argv[])
